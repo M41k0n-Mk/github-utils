66141b2bf626c3256f2dfdc8d90ac96025758397
M41k0n <142348031+M41k0n-Mk@users.noreply.github.com>
2024-12-24 17:13:49 -0300

    feat: implemented a feature that unfollows everyone you follow but who doesn't follow you back
    •  Created a method to make DELETE requests
    
    •  Extracted constant to an enum class for more readable and reusable code
    
    •  Configured interface with generics to enable different return types without duplicating code

--- src/main/java/me/m41k0n/actions/ExitApplicationAction.java
+++ src/main/java/me/m41k0n/actions/ExitApplicationAction.java
@@ -1,8 +1,9 @@
 package me.m41k0n.actions;
 
-public class ExitApplicationAction implements MenuAction {
+public class ExitApplicationAction implements MenuAction<Void> {
     @Override
-    public void execute() {
+    public Void execute() {
         System.out.println("Saindo da aplicação...");
+        return null;
     }
 }
--- src/main/java/me/m41k0n/service/APIConsume.java
+++ src/main/java/me/m41k0n/service/APIConsume.java
@@ -14,13 +14,33 @@ public class APIConsume {
         this.client = client;
     }
 
-    public String getData(String url) {
-        HttpRequest request = HttpRequest.newBuilder()
+    private HttpRequest.Builder createRequestBuilder(String url) {
+        return HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .header("Accept", "application/vnd.github+json")
                 .header("Authorization", "Bearer " + TOKEN)
-                .header("X-GitHub-Api-Version", "2022-11-28")
-                .build();
+                .header("X-GitHub-Api-Version", "2022-11-28");
+    }
+
+    public String getData(String url) {
+        HttpRequest request = createRequestBuilder(url).build();
+
+        HttpResponse<String> response;
+
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            throw new RuntimeException("A requisição HTTP falhou", e);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("A thread foi interrompida durante a request HTTP", e);
+        }
+
+        return response.body();
+    }
+
+    public String deleteData(String url) {
+        HttpRequest request = createRequestBuilder(url).DELETE().build();
 
         HttpResponse<String> response;
 
